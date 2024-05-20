1c24d8e3fdb007fabb2c8b9c64b9790f3d265ab1
Peter Cho <petercho42@gmail.com>
2024-05-20 19:05:36 -0400

    tee

--- teetimebot/management/commands/insert_test_data.py
+++ teetimebot/management/commands/insert_test_data.py
@@ -311,7 +311,7 @@ class Command(BaseCommand):
         user_request = UserTeeTimeRequest.objects.create(
             user=u,
             course=bethpage,
-            date=date(2024, 5, 19),
+            date=date(2024, 5, 24),
             tee_time_min=time(8, 59),
             tee_time_max=time(12, 1),
             search_time_min=time(7, 30),
